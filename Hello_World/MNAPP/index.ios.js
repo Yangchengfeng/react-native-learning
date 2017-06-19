e69e908cb1e86c37f15e42c441d4d645c8c521c4
yangchengfeng <linmeinicoder@gmail.com>
2017-06-19 14:28:34 +0800

    update:增加tabBar切换

--- Hello_World/MNAPP/index.ios.js
+++ Hello_World/MNAPP/index.ios.js
@@ -14,18 +14,71 @@ import {
 } from 'react-native';
 
 export default class MNAPP extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'homepage',
+    };
+  }
   render() {
     return (
-      <TabBarIOS>
-        <TabBarIOS.Item title="首页" icon={require('./myIcon/lf_tabbar_home.png')} selectedIcon={require('./myIcon/lf_tabbar_home_selected.png')}>
+      <TabBarIOS  unselectedTintColor="black"
+                  tintColor="red"
+                  barTintColor= 'white'>
+        <TabBarIOS.Item title="首页"
+                        selected={this.state.selectedTab === 'homepage'}
+                        icon={require('./myIcon/lf_tabbar_home.png')}
+                        selectedIcon={require('./myIcon/lf_tabbar_home_selected.png')}
+                        renderAsOriginal={true}
+                        onPress={() => {
+                          this.setState({
+                            selectedTab: 'homepage'
+                          });}}>
+          <View></View>
         </TabBarIOS.Item>
-        <TabBarIOS.Item title="微淘">
+        <TabBarIOS.Item title="微淘"
+                        selected={this.state.selectedTab === 'weitao'}
+                        icon={require('./myIcon/lf_tabbar_order.png')}
+                        selectedIcon={require('./myIcon/lf_tabbar_order_selected.png')}
+                        renderAsOriginal={true}
+                        onPress={() => {
+                          this.setState({
+                            selectedTab: 'weitao'
+                          });}}>
+          <View></View>
         </TabBarIOS.Item>
-        <TabBarIOS.Item title="社区">
+        <TabBarIOS.Item title="社区"
+                        selected={this.state.selectedTab === 'shequ'}
+                        icon={require('./myIcon/lf_tabbar_order.png')}
+                        selectedIcon={require('./myIcon/lf_tabbar_order_selected.png')}
+                        renderAsOriginal={true}
+                        onPress={() => {
+                          this.setState({
+                            selectedTab: 'shequ'
+                          });}}>
+           <View></View>
         </TabBarIOS.Item>
-        <TabBarIOS.Item title="购物车">
+        <TabBarIOS.Item title="购物车"
+                        selected={this.state.selectedTab === 'cart'}
+                        icon={require('./myIcon/lf_tabbar_cart.png')}
+                        selectedIcon={require('./myIcon/lf_tabbar_cart_selected.png')}
+                        renderAsOriginal={true}
+                        onPress={() => {
+                          this.setState({
+                            selectedTab: 'cart'
+                          });}}>
+          <View></View>
         </TabBarIOS.Item>
-        <TabBarIOS.Item title="我的淘宝">
+        <TabBarIOS.Item title="我的淘宝"
+                        selected={this.state.selectedTab === 'mine'}
+                        icon={require('./myIcon/lf_tabbar_order.png')}
+                        selectedIcon={require('./myIcon/lf_tabbar_order_selected.png')}
+                        renderAsOriginal={true}
+                        onPress={() => {
+                          this.setState({
+                            selectedTab: 'mine'
+                          });}}>
+          <View></View>
         </TabBarIOS.Item>
       </TabBarIOS>
     );
